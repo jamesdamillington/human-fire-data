27ce6b0b5fcdb9e70052e7da1181653f0fc7c9fa
James Millington <jamesdamillington@gmail.com>
2025-03-13 09:39:34 +0000

    reading data

--- mapping.r
+++ mapping.r
@@ -1,22 +1,47 @@
 library(readxl)
+library(dplyr)
+library(foreign)  #to read .dbf
 library(sf)
 
+
 #DAFI data from https://doi.org/10.6084/m9.figshare.c.5290792.v4
-DAFI.dat <- read_excel("data\\DAFI version 1.01.xlsx", sheet = "Land use")
+DAFI.case <- read_excel("data\\DAFI version 1.01.xlsx", sheet = "Record info")
+DAFI.use <- read_excel("data\\DAFI version 1.01.xlsx", sheet = "Land use")
+DAFI.gov <- read_excel("data\\DAFI version 1.01.xlsx", sheet = "Fire Policy")
+
+#col_type for DAFI.prev
+DAFI.prev.ct <- c('text', 'skip',  'skip', 'skip', 'skip', 'skip', 'skip',
+                  'numeric', 'skip', 'numeric', 'skip', 'numeric', 'skip', 'skip')
+
+DAFI.prev <- read_excel("data\\DAFI version 1.01.xlsx", sheet = "Fire suppression", na='ND', col_types=DAFI.prev.ct)
+
+
+  
+
 
 
 #LIFE data from https://doi.org/10.17637/rh.c.5469993
-LIFE.dat <- read_excel("data\\Database_V6.xlsx", sheet = "Case")
+LIFE.case <- read_excel("data\\Database_V6.xlsx", sheet = "Case")
+LIFE.gov <- read_excel("data\\Database_V6.xlsx", sheet = "Governance")  #column TY
+LIFE.prev <- read_excel("data\\Database_V6.xlsx", sheet = "Fire practices")  #column COTYPE
+
+
 
 #GFUS data from https://doi.org/10.5281/zenodo.10671047
 GFUS.shp <- st_read("data\\Shiny_app\\data\\Meta_data.shp")
 plot(st_geometry(GFUS.shp))
 
 
-GFUS.use <- st_read("data\\Shiny_app\\data\\Overall_fire_use_shape.shp")
+#GFUS.use <- st_read("data\\Shiny_app\\data\\Overall_fire_use_shape.shp")
+GFUS.use <- read.dbf("data\\Shiny_app\\data\\SH_fire_use_LIFE_SURVEY.dbf")
+GFUS.use <- as_tibble(GFUS.use)
 
-#GFUS user type:  columns we want: Q4a_1	Q4a_2	Q4a_3 from fire_use_shape (for User Type)
-#GFUS fire use: There are 26 specific fire uses in SH_fire_use_LIFE_SURVEY.shp  but 30 in GFUS methods summary.docx  (which were dropped/combined?)
-#GFUS governance:
+glimpse(GFUS.use)
+
+#lookup tables to create consistent classification
+lookup.use <- read_excel("DB-lookups.xlsx", sheet = "Practices")
+lookup.gov <- read_excel("DB-lookups.xlsx", sheet = "Governance")
+lookup.prev <- read_excel("DB-lookups.xlsx", sheet = "Prevention")
+
 
 
